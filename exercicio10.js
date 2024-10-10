e0402632c8458bf33d23314bed2e70f0ec91acd3
Gian F Maciel <gian.f.maciel@gmail.com>
2024-10-10 19:32:48 -0300

    commit

--- exercicio10.js
+++ exercicio10.js
@@ -1,4 +1,6 @@
 let dadosPessoais = (nome, idade, cidade, profissao ) => {
-
+    console.log(`Eu sou ${nome}`, `tenho ${idade} anos`, `moro em ${cidade}`, `e sou ${profissao}.`)
 
 }
+
+dadosPessoais("Gian", "33", "São Leopoldo", "Conferente")
---
+++ exercicio11.js
@@ -0,0 +1,9 @@
+let somas = (numero1, numero2) => {
+
+    return numero1 + numero2
+
+
+}
+const resultado = somas(10, 20)
+
+console.log(resultado)
